1867708cb160813620eec3952b368732d4fe5db5
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-05 17:31:26 +0600

    icon added xmark

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Link from './Link/Link';
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const NavBar = () => {
     const [open, setOpen] = useState(false);
@@ -13,8 +13,10 @@ const NavBar = () => {
     ]
     return (
         <nav>
-            <Bars3Icon onClick={() => { setOpen(!open) }} className="h-6 w-6" />
-            <span>{open ? 'open' : 'close'}</span>
+            {
+                open ? <XMarkIcon onClick={() => { setOpen(!open) }} className="h-6 w-6" /> : <Bars3Icon onClick={() => { setOpen(!open) }} className="h-6 w-6" />
+            }
+
             <ul className='md: flex justify-center'>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
