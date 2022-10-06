46f2c37687dacbe89ef02ba6e542132147785c75
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-06 14:16:31 +0600

    added rechart

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Pricing from './Components/NavBar/Pricing/Pricing';
+import AssignmentMarks from './Components/AssignmentMarks/AssignmentMarks';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <h1 className='font-bold text-4xl'>This is big big header</h1>
-
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
